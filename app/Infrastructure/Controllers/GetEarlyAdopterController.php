88b4bbef5418a49db8650d02d5ba0c91394d5db0
iñaki <luque.126265@e.unavarra.es>
2023-04-10 08:35:05 +0000

    TDD GET /api/earlyAdopter/{userEmail} UserIsEarlyAdopter

--- app/Infrastructure/Controllers/GetEarlyAdopterController.php
+++ app/Infrastructure/Controllers/GetEarlyAdopterController.php
@@ -2,17 +2,37 @@
 
 namespace App\Infrastructure\Controllers;
 
+use App\Application\UserDataSource\UserDataSource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class GetEarlyAdopterController extends BaseController
 {
-    public function __invoke(): JsonResponse
+    private UserDataSource $userDataSource;
+
+    /**
+     * @param UserDataSource $userDataSource
+     */
+    public function __construct(UserDataSource $userDataSource)
     {
-        return response()->json(
-            ['error' => 'usuario no encontrado'],
-            Response::HTTP_NOT_FOUND
-        );
+        $this->userDataSource = $userDataSource;
+    }
+
+    public function __invoke(string $userEmail): JsonResponse
+    {
+        $user = $this->userDataSource->findByEmail($userEmail);
+        if (is_null($user)) {
+            return response()->json(
+                ['error' => 'usuario no encontrado'],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+        if ($user->getId() >= 1000) {
+            return response()->json(
+                ['El usuario es early adopter'],
+                Response::HTTP_OK
+            );
+        }
     }
 }
--- tests/app/Infrastructure/Controller/GetEarlyAdopterControllerTest.php
+++ tests/app/Infrastructure/Controller/GetEarlyAdopterControllerTest.php
@@ -2,18 +2,52 @@
 
 namespace Tests\app\Infrastructure\Controller;
 
+use App\Application\UserDataSource\UserDataSource;
+use App\Domain\User;
 use Tests\TestCase;
+use Mockery;
 
 class GetEarlyAdopterControllerTest extends TestCase
 {
+    private UserDataSource $userDataSource;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->userDataSource = Mockery::mock(UserDataSource::class);
+        $this->app->bind(UserDataSource::class, function () {
+            return $this->userDataSource;
+        });
+    }
+
+
     /**
      * @test
      */
     public function userIsNotFound()
     {
+        $this->userDataSource
+            ->expects("findByEmail")
+            ->with('email@email.com')
+            ->andReturnNull();
         $response = $this->get("/api/earlyAdopter/email@email.com");
 
         $response->assertNotFound();
         $response->assertExactJson(['error' => 'usuario no encontrado']);
+    }
+
+    /**
+     * @test
+     */
+    public function userIsEarlyAdopter()
+    {
+        $this->userDataSource
+            ->expects("findByEmail")
+            ->with('email@email.com')
+            ->andReturn(new User(1001, 'email@email.com'));
+        $response = $this->get("/api/earlyAdopter/email@email.com");
+
+        $response->assertOk();
+        $response->assertExactJson(['El usuario es early adopter']);
     }
 }
